d4f1d10acf7b56d636c2e8972fe2afeb159487a7
Divyam Bansal <divyam.bansal2000@gmail.com>
2024-04-20 16:40:29 -0500

    mail

--- pages/api/send/route.js
+++ pages/api/send/route.js
@@ -1,31 +1,35 @@
 import nodemailer from 'nodemailer';
 
-export async function post(req, res) {
-  const { email, subject, message } = req.body;
-  if (!email || !subject || !message) {
-    return res.status(400).json({ message: 'Missing fields' });
-  }
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, subject, message } = req.body;
+    if (!email || !subject || !message) {
+      return res.status(400).json({ message: 'Missing fields' });
+    }
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-  const mailOptions = {
-    from: email, // sender address
-    to: process.env.YOUR_EMAIL_ADDRESS, // your email address
-    subject: subject, // Subject from the form
-    text: message, // message from the form
-  };
+    const mailOptions = {
+      from: email,
+      to: process.env.YOUR_EMAIL_ADDRESS,
+      subject: subject,
+      text: message,
+    };
 
-  try {
-    const result = await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email sent successfully', result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error sending email', error: error.message });
+    try {
+      const result = await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email sent successfully', result });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error sending email', error: error.message });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
   }
 }
